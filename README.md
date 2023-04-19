dce7d03d3b8d93cab64ade326751efba5c3ca0e0
yangtao01 <yangtao01@zhihu.com>
2023-04-19 16:44:58 +0800

    add file

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A simple userscript to keep chat.openai.com alive.
 
 ## Install
 
-- https://github.com/sorrycc/keep-chatgpt-simple/raw/master/keep.user.js
+- https://github.com/taotao9125rycc/keep-chatgpt-simple/raw/master/keep.user.js
 
 ## Inspired by
 
 - https://github.com/xcanwin/KeepChatGPT
+- https://github.com/sorrycc/keep-chatgpt-simple
--- keep.user.js
+++ keep.user.js
@@ -11,9 +11,10 @@
 
 (() => {
   let apiUrl = '/api/auth/session';
+  let timer = -1;
 
-  function getRandomNumber() {
-    return Math.floor(Math.random() * 11) + 20;
+  function getRandomNumber(threshold) {
+    return threshold;
   }
 
   async function keepChat() {
@@ -22,23 +23,36 @@
     const text = await res.text();
     const isJSON = contentType && contentType.includes('application/json');
     const is403 = res.status === 403;
-    console.log('[Keep ChatGPT Simple] >', isJSON, is403, text);
+
     const isValid = isJSON && !is403 && text.includes('expires');
     if (!isValid) {
       throw new Error('Invalid response');
     }
   }
 
-  function run() {
-    keepChat().then(() => {
-      const random = getRandomNumber();
-      console.log('[Keep ChatGPT Simple] Next run in ' + random + ' seconds');
-      setTimeout(run, random * 1000);
-    }).catch(e => {
-      console.error(e.message);
-      alert(`Session 失效了，请手动刷新页面重试！`);
-    });
+  document.addEventListener('visibilitychange', () => {
+     var isHidden = document.hidden;
+      if (isHidden) {
+          run(10);
+      } else {
+          run(20);
+      }
+
+  });
+
+
+  function run(threshold) {
+    clearTimeout(timer);
+    const random = getRandomNumber(threshold);
+    timer = setTimeout(async () => {
+        try {
+            await keepChat();
+            run(threshold)
+        } catch(e) {
+            console.error(e.message)
+        }
+    }, threshold * 1000)
   }
 
-  run();
+
 })();
